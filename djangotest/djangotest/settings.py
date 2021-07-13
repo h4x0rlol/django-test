ad04ca59c3e6a6a203c5660df8b211d7f9d2b991
h4x0rlol <mazafaker.com@gmail.com>
2021-07-13 21:19:44 +0600

    refactor

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,11 +83,11 @@ WSGI_APPLICATION = 'djangotest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_test',
-        'USER': 'dh',
-        'PASSWORD': '1337',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
--- djangotest/main/views.py
+++ djangotest/main/views.py
@@ -1,13 +1,13 @@
+import os
+from main.tcp_handler import tcp_connect
 from django.shortcuts import render
 from .models import List
-import socket
 from django.core import serializers
 
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-# MESSAGE = "Hello, World!".encode()
 lists = List.objects.all()
 
 
@@ -17,12 +17,10 @@ def index(request):
 
 def send_data(request):
     if request.method == "GET":
-        MESSAGE = serializers.serialize("xml", lists).encode()
-        print(MESSAGE)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((TCP_IP, TCP_PORT))
-        s.send(MESSAGE)
-        data = s.recv(BUFFER_SIZE)
-        s.close()
+        MESSAGE = "keklol".encode()
+        con = tcp_connect(TCP_IP, TCP_PORT)
+        con.send(MESSAGE)
+        data = con.recv(BUFFER_SIZE)
         print("received data:", data)
+
     return render(request, 'main/index.html', {"lists": lists})
